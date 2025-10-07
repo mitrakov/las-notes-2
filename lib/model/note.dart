0d7d924c4d55b96c426fea1d4386df40cb2ce654
Tommy-Air <tommy@air.ru>
2025-10-07 14:49:37 +0300

    Move project codebase

---
+++ lib/model/note.dart
@@ -0,0 +1,8 @@
+class Note {
+  final int id;
+  final String data;
+  final String tags;
+  final bool isDeleted;
+
+  Note({required this.id, required this.data, required this.tags, required this.isDeleted});
+}
